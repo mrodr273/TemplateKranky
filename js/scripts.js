404d077c1026847acae70e2e77d041306171f20c
mrodr273 <47089416+mrodr273@users.noreply.github.com>
2019-03-03 19:56:30 -0800

    Kranky templatef from scratch

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+  //fix links hyperlinks
+  /*$('a#home').click(function() {
+    window.location = 'layout.html#jumpHere1';
+  });
+  $('a#about').click(function() {
+    window.location = 'layout.html#jumpHere2';
+  });
+  $('a#services').click(function() {
+    window.location = 'layout.html#jumpHere3';
+  });
+  $('a#contact').click(function() {
+    window.location = 'layout.html#jumpHere4';
+  });*/
+});
+/*$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});*/
+
+/*$(".my-class").show();
+$(".my-class").hide();
+$(".my-class").toggle();*/
+
+/*Add a CSS class to an element:
+$("body").addClass("my-class");*/
+
+/*var add = function(number1, number2) {
+  return number1 + number2;
+};*/
+
+/*var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));*/
+
+/*$('.element-to-select').click(function() {
+  alert('hi');
+});*/
+
+/*Test that you've selected the correct DOM element:
+$('.element-to-select').css('background-color', 'green');*/
+
+/*.clickable {
+  cursor: pointer;
+  color: #0088cc;
+.clickable:hover {
+  text-decoration: underline;
+}*/
